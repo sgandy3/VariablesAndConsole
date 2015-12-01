22a84af6c5c0e915a9ab58bde72158eac13626c6
Your Name <you@example.com>
2015-12-01 07:36:14 -0500

    c# variables and console

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paperWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 128;
+            byte b = 254;
+            sbyte c = -127;
+           int d = -1500123;
+            byte e = 1;
+           long f = 200000000;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}." ,a,b,c,d,e,f);
+        }
+    }
+}
